1b7edc654ffd3e3359f44f001dfed193fa290eb1
Ritesh <ritesh2897pandey@gmail.com>
2022-09-20 20:12:32 +0530

    quiz api

---
+++ quiz/serializer.py
@@ -0,0 +1,48 @@
+from rest_framework import serializers
+from .models import *
+
+
+class QuizSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Quizzes
+        fields = [
+            'title',
+        ]
+
+
+class AnswerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        
+        model = Answer
+        fields = [
+            'id',
+            'answer_text',
+            'is_right',
+        ]
+
+
+class RandomQuestionSerializer(serializers.ModelSerializer):
+
+    answer = AnswerSerializer(many=True, read_only=True)
+
+    class Meta:
+    
+        model = Question
+        fields = [
+            'question','answer',
+        ]
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+
+    answer = AnswerSerializer(many=True, read_only=True)
+    quiz = QuizSerializer(read_only=True)
+
+    class Meta:
+    
+        model = Question
+        fields = [
+            'quiz','question','answer',
+        ]
